7932069f0406b5298fe8150a2ed44a0554458c9b
Steve <ostylehouse@gmail.com>
2024-03-26 21:10:07 +1300

    these do nothing

--- apps/sveltekit-app/vite.config.ts
+++ apps/sveltekit-app/vite.config.ts
@@ -31,10 +31,6 @@ export default defineConfig({
 		exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
 	  },
 	  server: {
-		headers: {
-		  'Cross-Origin-Opener-Policy': 'same-origin',
-		  'Cross-Origin-Embedder-Policy': 'require-corp'
-		},
 		fs: {
 			allow: ['../..']
 		}
